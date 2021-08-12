5bea8f4b6cee33057cee5ea4a917be4c2e1e2d8b
fco-felix <felix@solsistemas.com>
2021-08-11 23:00:16 -0300

    Entrega projeto final

--- script.js
+++ script.js
@@ -1,59 +1,112 @@
+let mordida = new Audio('mordida.mp3');
+let placar = document.getElementById("titulo");
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-let box = 32;
-let snake = [];
-snake[0] = {
-    x: 8 * box,
-    y: 8 * box
+let tamanho = 32;
+let pontuacao = 0;
+let cobrinha = [];
+criarCobrinha();
+let direcao = "d";
+let comida = {
+    x: posicaoAleatoria(),
+    y: posicaoAleatoria()
 }
-let direction = "rigth";
+function posicaoAleatoria() {
+    return Math.floor(Math.random() * 15 + 1) * tamanho;
+}
 
 function criarBG() {
-    context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+    context.fillStyle = "tan";
+    context.fillRect(0, 0, 16 * tamanho, 16 * tamanho);
+    placar.innerHTML = "Jogo da Cobrinha => "+pontuacao+" pontos";
 }
 
 function criarCobrinha() {
-    for (i = 0; i < snake.length; i++) {
+    cobrinha = [];
+    cobrinha[0] = {
+        x: 8 * tamanho,
+        y: 8 * tamanho
+    }
+}
+
+function pintarCobrinha() {
+    for (i = 0; i < cobrinha.length; i++) {
         context.fillStyle = "green";
-        context.fillRect(snake[i].x, snake[i].y, box, box);
+        context.fillRect(cobrinha[i].x, cobrinha[i].y, tamanho, tamanho);
     }
 }
 
-document.addEventListener('keydown', update);
-
-function update(event){
-    if (event.keyCode == 37 && direction != "r") direction = "l";
-    if (event.keyCode == 38 && direction != "d") direction = "u";
-    if (event.keyCode == 39 && direction != "l") direction = "r";
-    if (event.keyCode == 40 && direction != "u") direction = "d";
+function criarComida() {
+    comida.x = posicaoAleatoria();
+    comida.y = posicaoAleatoria();
 }
 
-function iniciarJogo(){
-    if (snake[0].x < 0 && direction == "l") snake[0].x = 16 * box;
-    if (snake[0].y < 0 && direction == "u") snake[0].y = 16 * box;
-    if (snake[0].x > 15 * box && direction == "r") snake[0].x = 0;
-    if (snake[0].y > 15 * box && direction == "d") snake[0].y = 0;
+function adicionarComida() {
+    context.fillStyle = "red";
+    context.fillRect(comida.x, comida.y, tamanho, tamanho);
+}
 
+document.addEventListener('keydown', movimento);
+
+function movimento(event) {
+    if (event.keyCode == 37 && direcao != "d") direcao = "e";
+    if (event.keyCode == 38 && direcao != "b") direcao = "c";
+    if (event.keyCode == 39 && direcao != "e") direcao = "d";
+    if (event.keyCode == 40 && direcao != "c") direcao = "b";
+}
+
+function comer(){
+    pontuacao++;
+    criarComida();
+    mordida.play();
+}
+function finalJogo() {
+    clearInterval(jogo);
+    let resposta = window.confirm("Fim de jogo! Você fez "+pontuacao+" pontos. Reiniciar ?")
+    if (resposta) {
+        criarCobrinha();
+        jogo = setInterval(iniciarJogo, 200);
+        pontuacao = 0;
+    }
+
+}
+
+function iniciarJogo() {
+    if (cobrinha[0].x < 0 && direcao == "e") cobrinha[0].x = 16 * tamanho;
+    if (cobrinha[0].y < 0 && direcao == "c") cobrinha[0].y = 16 * tamanho;
+    if (cobrinha[0].x > 15 * tamanho && direcao == "d") cobrinha[0].x = 0;
+    if (cobrinha[0].y > 15 * tamanho && direcao == "b") cobrinha[0].y = 0;
+
+    for (posicao = 3; posicao < cobrinha.length; posicao++) {
+        if (cobrinha[0].x == cobrinha[posicao].x && cobrinha[0].y == cobrinha[posicao].y) {
+            finalJogo();
+        }
+    }
     criarBG();
-    criarCobrinha();
+    pintarCobrinha();
+    adicionarComida();
 
-    let snakeX = snake[0].x;
-    let snakeY = snake[0].y;
+    let cobrinhaX = cobrinha[0].x;
+    let cobrinhaY = cobrinha[0].y;
 
-    if (direction == 'r') snakeX += box;
-    if (direction == 'l') snakeX -= box;
-    if (direction == 'u') snakeY -= box;
-    if (direction == 'd') snakeY += box;
+    if (direcao == 'd') cobrinhaX += tamanho;
+    if (direcao == 'e') cobrinhaX -= tamanho;
+    if (direcao == 'c') cobrinhaY -= tamanho;
+    if (direcao == 'b') cobrinhaY += tamanho;
 
-    snake.pop();
-
-    let newHead = {
-        x: snakeX,
-        y: snakeY
+    if (cobrinhaX != comida.x || cobrinhaY != comida.y) {
+        cobrinha.pop();
+    } else {
+        comer();
     }
 
-    snake.unshift(newHead);
+    let novaCabeca = {
+        x: cobrinhaX,
+        y: cobrinhaY
+    }
+
+    cobrinha.unshift(novaCabeca);
 }
 
-let jogo = setInterval(iniciarJogo,200);
+alert("Vamos começar!");
+let jogo = setInterval(iniciarJogo, 200);
